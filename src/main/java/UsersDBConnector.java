b3b63e487e73dd8e9eff2d9de2677f5b8b1420c6
bubu <nchik12@freeuni.edu.ge>
2020-07-04 18:30:55 +0400

    uppercase DATABASE

--- src/main/java/UsersDBConnector.java
+++ src/main/java/UsersDBConnector.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class UsersDBConnector {
 
     private Connection connection;
-    private String usersTableName = "users";
+    private final String usersTableName = "users";
     private HashMap<String, String> hashMap;
     private int usersCount;
 
