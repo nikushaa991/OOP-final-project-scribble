c32bbca3880ac405eaaa11bb2f27162e093dc3e0
mapatie deda <57991126+nikushaa991@users.noreply.github.com>
2020-07-05 14:07:38 +0400

    Canvas + socket prototype WIP

--- src/main/java/DatabaseCredentials.java
+++ src/main/java/DatabaseCredentials.java
@@ -2,7 +2,7 @@ package main.java;
 
 public class DatabaseCredentials {
     //ENTER YOUR OWN DATABASE CREDENTIALS
-    static final String url = "jdbc:mysql://localhost:3306/DB_NAME?useSSL=false";
+    static final String url = "jdbc:mysql://localhost:3306/SCRIBBLE?useSSL=false";
     static final String user = "root";
     static final String password = "991999";
 }
