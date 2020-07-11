7f42923de0d04cd089f596e577ebc5d7b9c3a3b9
mapatie deda <57991126+nikushaa991@users.noreply.github.com>
2020-07-11 13:51:16 +0400

    fixed gitignore

--- src/utils/DatabaseCredentials.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class DatabaseCredentials {
-    //ENTER YOUR OWN DATABASE CREDENTIALS
-    public static final String url = "jdbc:mysql://localhost:3306/SCRIBBLE?useSSL=false";
-    public static final String user = "root";
-    public static final String password = "1234";
-}
