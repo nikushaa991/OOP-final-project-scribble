9ed424f190d6fa3118646250f2b5477dc0d8fd96
bubu <nchik12@freeuni.edu.ge>
2020-07-18 18:57:09 +0400

    userDaoTest update

--- src/tests/UsersDAOTest.java
+++ src/tests/UsersDAOTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import databases.users.UsersDAO;
+import login.classes.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,13 +15,22 @@ class UsersDAOTest {
     /* Test basic functionality of the database */
     @Test
     public void test1() throws SQLException, NoSuchAlgorithmException {
-        String username = "testusername2";
+        String username = "testusername";
         String password = "molly";
         UsersDAO dao = new UsersDAO();
-        dao.newUser(username, password, 1000);
-        assertTrue(dao.passwordMatches(username, "molly"));
-        assertTrue(dao.exists(username));
-        Assertions.assertEquals(dao.nrow()-1, dao.getUser(username).getId());
-        dao.deleteUser(username);
+        try {
+            dao.newUser(username, password, 1000);
+            assertTrue(dao.passwordMatches(username, "molly"));
+            assertTrue(dao.exists(username));
+            Assertions.assertEquals(dao.nrow() - 1, dao.getUser(username).getId());
+            User testUser = dao.getUser(username);
+            assertTrue(testUser.getUsername().equals(username));
+
+            dao.updateRank(username, 10000);
+            User testUser2 = dao.getUser(username);
+            assertEquals(testUser2.getRating(), 10000);
+        } finally {
+            dao.deleteUser(username);
+        }
     }
 }
